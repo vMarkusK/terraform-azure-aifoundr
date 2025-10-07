fe6ac71fdf1f04401a7fa0fee9de1e0f99a6edac
Markus Kraus <markus.kraus@gmail.com>
2025-10-07 21:00:01 +0200

    fix legacy limitation

--- aifoudry.tf
+++ aifoudry.tf
@@ -71,12 +71,12 @@ module "ai_foundry" {
     }
   }
 
-  depends_on = [azapi_resource_action.purge_ai_foundry]
+  #depends_on = [azapi_resource_action.purge_ai_foundry]
 }
 
-resource "azapi_resource_action" "purge_ai_foundry" {
-  method      = "DELETE"
-  resource_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.CognitiveServices/locations/${azurerm_resource_group.this.location}/resourceGroups/${azurerm_resource_group.this.name}/deletedAccounts/${module.naming.cognitive_account.name_unique}"
-  type        = "Microsoft.Resources/resourceGroups/deletedAccounts@2021-04-30"
-  when        = "destroy"
-}
+# resource "azapi_resource_action" "purge_ai_foundry" {
+#   method      = "DELETE"
+#   resource_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.CognitiveServices/locations/${azurerm_resource_group.this.location}/resourceGroups/${azurerm_resource_group.this.name}/deletedAccounts/${module.naming.cognitive_account.name_unique}"
+#   type        = "Microsoft.Resources/resourceGroups/deletedAccounts@2021-04-30"
+#   when        = "destroy"
+# }
