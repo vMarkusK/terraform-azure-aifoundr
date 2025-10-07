7b9557083d815b86b4c4af9f0cd6260991cb5bf4
Markus Kraus <markus.kraus@gmail.com>
2025-10-07 21:02:42 +0200

    fix source

--- aifoudry.tf
+++ aifoudry.tf
@@ -1,5 +1,5 @@
 module "ai_foundry" {
-  source  = "Azure/avm-ptn-aiml-ai-foundry/azurerm//examples/standard-public"
+  source  = "Azure/avm-ptn-aiml-ai-foundry/azurerm"
   version = "0.6.0"
 
   base_name                  = var.usecase
@@ -71,12 +71,12 @@ module "ai_foundry" {
     }
   }
 
-  #depends_on = [azapi_resource_action.purge_ai_foundry]
+  depends_on = [azapi_resource_action.purge_ai_foundry]
 }
 
-# resource "azapi_resource_action" "purge_ai_foundry" {
-#   method      = "DELETE"
-#   resource_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.CognitiveServices/locations/${azurerm_resource_group.this.location}/resourceGroups/${azurerm_resource_group.this.name}/deletedAccounts/${module.naming.cognitive_account.name_unique}"
-#   type        = "Microsoft.Resources/resourceGroups/deletedAccounts@2021-04-30"
-#   when        = "destroy"
-# }
+resource "azapi_resource_action" "purge_ai_foundry" {
+  method      = "DELETE"
+  resource_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.CognitiveServices/locations/${azurerm_resource_group.this.location}/resourceGroups/${azurerm_resource_group.this.name}/deletedAccounts/${module.naming.cognitive_account.name_unique}"
+  type        = "Microsoft.Resources/resourceGroups/deletedAccounts@2021-04-30"
+  when        = "destroy"
+}
