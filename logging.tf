d8345842e6ebce4a9ec73709133d4d06251fb720
Markus Kraus <markus.kraus@gmail.com>
2025-10-07 20:53:48 +0200

    public example

---
+++ logging.tf
@@ -0,0 +1,7 @@
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = module.naming.log_analytics_workspace.name_unique
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  retention_in_days   = 30
+  sku                 = "PerGB2018"
+}
